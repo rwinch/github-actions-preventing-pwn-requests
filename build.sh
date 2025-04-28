761e0644f42932f0e4dffbd2edaab554857a6e54
Rob Winch <362503+rwinch@users.noreply.github.com>
2025-04-28 15:31:14 -0500

    echo GITHUB_TOKEN

--- build.sh
+++ build.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 echo "Build it!"
-printenv | sort
-if [ -n "$GITHUB_TOKEN" ]
-then
-  # printenv will mask the GITHUB_TOKEN, so this proves it was there
-  echo "pwned!"
-else
-  echo "GITHUB_TOKEN is not found"
-fi
+echo "My token is: $GITHUB_TOKEN"
+
