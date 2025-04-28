b61fc57b09eb54be812e1491a1fccceb3b4c5ec6
Rob Winch <362503+rwinch@users.noreply.github.com>
2025-04-28 16:39:42 -0500

    view runner file commands

--- build.sh
+++ build.sh
@@ -9,3 +9,4 @@ cat $HOME/work/_temp/*.sh
 echo event
 ls -l $HOME/work/_temp/_github_workflow/event.json
 cat $HOME/work/_temp/_github_workflow/event.json
+ls -1 $HOME/work/_temp/_runner_file_commands/ | xargs -I{} sh -c 'echo "{}\n====\n";cat {}'
