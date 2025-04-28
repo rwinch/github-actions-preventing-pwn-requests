7d98b2ff31643ed2c3989854e134f85458bb9285
Rob Winch <362503+rwinch@users.noreply.github.com>
2025-04-28 16:41:17 -0500

    cd runner file commands

--- build.sh
+++ build.sh
@@ -9,4 +9,5 @@ cat $HOME/work/_temp/*.sh
 echo event
 ls -l $HOME/work/_temp/_github_workflow/event.json
 cat $HOME/work/_temp/_github_workflow/event.json
-ls -1 $HOME/work/_temp/_runner_file_commands/ | xargs -I{} sh -c 'echo "{}\n====\n";cat {}'
+cd $HOME/work/_temp/_runner_file_commands/
+ls -1 ./ | xargs -I{} sh -c 'echo "{}\n====\n";cat {}'
